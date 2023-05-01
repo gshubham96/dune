d67c3b9f35b38d6dfe6028df4d91ad837928601c
gshubham96 <gshubham96@gmail.com>
2023-05-01 15:14:31 +0200

    minor

--- src/Control/ASV/NmpcCourseController/Task.cpp
+++ src/Control/ASV/NmpcCourseController/Task.cpp
@@ -339,8 +339,8 @@ namespace Control
           std::map<std::string, double> params_d;
           params_d["Vc"] = 0.35;      params_d["beta_c"] = 1.57;
           params_d["Vw"] = 5;         params_d["beta_w"] = 1.57;
-          params_d["k_1"] = 0.9551;   params_d["k_2"] = -0.031775;
           params_d["Q"] = 4.5;        params_d["R"] = 1.5;
+          params_d["Hs"] = 5; params_d["omega_p"] = 0.6283; params_d["gamma_p"] = 1.57;
           controller.updateMpcParams(params_d);
           while (!stopping())
           {

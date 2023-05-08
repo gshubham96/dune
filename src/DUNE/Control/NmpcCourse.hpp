a5de1a023534c821f86d8030783d54edc68e417c
gshubham96 <gshubham96@gmail.com>
2023-05-08 11:56:33 +0200

    minor

--- src/DUNE/Control/NmpcCourse.hpp
+++ src/DUNE/Control/NmpcCourse.hpp
@@ -137,7 +137,7 @@ namespace DUNE
 
                 // configures the class. 
                 // TODO make this a constructor instead?
-                bool configureSolver(std::string model_type, std::string cost_type, double Tp, double Ts, bool compile);
+                bool configureSolver(std::string model_type, std::string cost_type, std::string file_path, double Tp, double Ts, bool compile);
 
             // Default Constructor
             NmpcCourse();
